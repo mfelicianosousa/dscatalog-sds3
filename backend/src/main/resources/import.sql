b4032eb4cc4a784be5525c13148f41e839257abe
mfelicianosousa <marcelino.feliciano@hotmail.com>
2022-02-15 17:46:10 -0400

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_category(name) VALUES ('Books');
+INSERT INTO tb_category(name) VALUES ('Electronics');
+INSERT INTO tb_category(name) VALUES ('Computers');
+INSERT INTO tb_category(name) VALUES ('Som');
+INSERT INTO tb_category(name) VALUES ('Videos');
+INSERT INTO tb_category(name) VALUES ('Celulares');
+INSERT INTO tb_category(name) VALUES ('Notebooks');
+INSERT INTO tb_category(name) VALUES ('Memórias');
+INSERT INTO tb_category(name) VALUES ('Hds, ssd');
