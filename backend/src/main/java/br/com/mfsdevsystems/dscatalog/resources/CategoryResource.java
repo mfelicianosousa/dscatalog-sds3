5bfd2ead05ec6941c449eac23d71750902b62138
mfelicianosousa <marcelino.feliciano@hotmail.com>
2022-02-19 15:50:55 -0400

    Post new Category

--- backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/CategoryResource.java
@@ -1,16 +1,19 @@
 package br.com.mfsdevsystems.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.mfsdevsystems.dscatalog.dto.CategoryDTO;
-import br.com.mfsdevsystems.dscatalog.entities.Category;
 import br.com.mfsdevsystems.dscatalog.services.CategoryService;
 
 @RestController
@@ -38,6 +41,16 @@ public class CategoryResource {
 		CategoryDTO dto = service.findById(id );
 		
 		return ResponseEntity.ok().body( dto ) ;
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto ){
+		
+		dto = service.insert( dto ) ;
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body( dto ) ;
+		
 	}
 
 }
