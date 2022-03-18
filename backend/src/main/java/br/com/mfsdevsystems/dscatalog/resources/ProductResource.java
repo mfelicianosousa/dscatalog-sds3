e53c2a2214642985ce81d388c10b0f446757d425
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-03-17 21:21:10 -0400

    Basic beans validation

--- backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/ProductResource.java
@@ -2,11 +2,11 @@ package br.com.mfsdevsystems.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -63,7 +63,7 @@ public class ProductResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert( @RequestBody ProductDTO dto ){
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto ){
 		
 		dto = service.insert( dto ) ;
 		
@@ -76,7 +76,7 @@ public class ProductResource {
 	
 	
 	@PutMapping(value="/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto ){
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto ){
 		
 		dto = service.update( id, dto ) ;
 		
