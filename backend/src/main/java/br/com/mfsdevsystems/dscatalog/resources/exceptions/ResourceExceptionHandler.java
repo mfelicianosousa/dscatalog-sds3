8d5f8389cec79636336dd5539df6613a05efc100
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-03-29 20:11:41 -0400

    Custom validation response

--- backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,7 +31,7 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(httpStatus).body(err);
 		
 	}
-
+	
 	@ExceptionHandler(DatabaseException.class)
 	public ResponseEntity<StandardError> databaseError(DatabaseException e, HttpServletRequest request){
 		HttpStatus httpStatus = HttpStatus.BAD_REQUEST ;
@@ -43,6 +45,27 @@ public class ResourceExceptionHandler {
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(httpStatus).body(err);
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validationError(MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY ;
+		
+		ValidationError err = new ValidationError();
+		
+		err.setTimestamp( Instant.now() );
+		err.setStatus( httpStatus.value());
+		err.setError( "Validation Exception");
+		err.setMessage( e.getMessage() );
+		err.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status( httpStatus ).body(err);
 		
 	}
 }
---
+++ backend/src/main/java/br/com/mfsdevsystems/dscatalog/resources/exceptions/ValidationError.java
@@ -0,0 +1,20 @@
+package br.com.mfsdevsystems.dscatalog.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 3647726326271131691L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message )) ;
+	}
+	
+}
